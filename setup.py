9f56704143943efcafa22f1f5cb515f7cc3b95a5
jpcurrea <johnpaulcurrea@gmail.com>
2021-07-28 15:13:37 -0400

    test with setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name="eye_tools",
+      version='0.1.0',
+      description='tools for image analysis, particularly for extracting\
+      parameters relevant to the optical performance of compound eyes.',
+      url="https://github.com/jpcurrea/eye_tools.git",
+      author='Pablo Currea',
+      author_email='johnpaulcurrea@gmail.com',
+      license='MIT',
+      packages=['eye_tools'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'scikit-image',
+          'pandas',
+          'pillow',
+          'pyqt5',
+      ],
+      zip_safe=False)
